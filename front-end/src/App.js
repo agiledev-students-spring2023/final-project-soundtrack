2053bf2eada9c7e6f125e149d8b812652455943e
Asukanami <al6695@nyu.edu>
2023-03-06 14:41:37 -0500

    Choose Song Updated

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,26 @@
-//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Post from "./post";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
+import "./Browse";
 
-const App = () => {
-  console.log("Rendering App");
+function App() {
   return (
-    <Post />
-
-
-
-   /* <Router>
-      <Routes>
-        <Route path="/" element={<Post />} />
-      </Routes>
-      </Router> */ 
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
-export default App
+export default App;
