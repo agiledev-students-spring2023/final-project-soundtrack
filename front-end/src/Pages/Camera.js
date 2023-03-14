4d9be51216766c29694eefe5470d02e7a19dd88e
AliceLong <al6695@nyu.edu>
2023-03-14 17:19:11 -0400

    Route main page and basic camera page layout

--- front-end/src/Pages/Camera.js
+++ front-end/src/Pages/Camera.js
@@ -1,15 +1,72 @@
-import React from 'react'
+
+import React, { useEffect, useRef } from "react";
+
 
 const Camera = () => {
-    console.log("Rendering Camera");
-    return <div>
-        <h2>
-        Take a pic
-        </h2>
-        <button>
-          Go to Location
-        </button>
-    </div>;
+  let videoRef = useRef(null);
+
+  let photoRef = useRef(null)
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true
+      })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const takePicture = () => {
+    const width = 400
+    const height = width / (16 / 9)
+    
+    let video = videoRef.current
+
+    let photo = photoRef.current
+
+    photo.width = width
+
+    photo.height = height
+
+    let ctx = photo.getContext('2d')
+
+    ctx.drawImage(video, 0, 0, width, height)
+    
+  }
+
+  const clearImage = () => {
+    let photo = photoRef.current
+
+    let ctx = photo.getContext('2d')
+
+    ctx.clearRect(0,0,photo.width,photo.height)
+  }
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+
+    return (
+      <div className="container">
+      <h1 className="text-center">Camera Selfie App in React</h1>
+
+      <video ref={videoRef} className="container"></video>
+
+      <button onClick={takePicture} className="btn btn-danger container">Take Picture</button>
+
+      <canvas className="container" ref={photoRef}></canvas>
+
+      <button onClick={clearImage} className="btn btn-primary container">Clear Image</button>
+
+      <br/><br/>
+    </div>
+    )
   };
 
 export default Camera;
