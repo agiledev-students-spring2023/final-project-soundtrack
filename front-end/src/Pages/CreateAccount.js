2eed3ba997e1079ea8e8fc77015bb4998f6c5aad
isabellaMas <icm268@nyu.edu>
2023-03-20 15:58:59 -0400

    adding inputs to create account page

--- front-end/src/Pages/CreateAccount.js
+++ front-end/src/Pages/CreateAccount.js
@@ -1,12 +1,43 @@
-import './Login.css';
+import './CreateAccount.css';
 
+function goToLogin(e) {
+    e.preventDefault();
+    window.location = './';
+}
+
+function handleSubmit(e){
+    e.preventDefault();
+    window.location = './Map';
+}
 
 function CreateAccount() {
   return (
-    <div className="Login">
-        <div className="logo">
-            <h1>SoundTack</h1>
-            <p>full logo to be added when developed</p>
+    <div className="createAccount">
+        <h2>Create Account</h2>
+        <div className="inputs-CreateAccount">
+            <form onSubmit={handleSubmit}>
+                <div className="input-containerCreateAccount">
+                    <input type="name" name="name" placeholder="Name" required />
+                </div>
+                <div className="input-containerCreateAccount">
+                    <input type="user" name="uname" placeholder="@username" required />
+                </div>
+                <div className="input-containerCreateAccount">
+                    <input type="password" name="pass" placeholder="password" required />
+                </div>
+                <div className="input-containerCreateAccount">
+                    <input type="mail" name="mail" placeholder="e-mail" required />
+                </div>
+                <div className="input-containerCreateAccount">
+                    <input type="spot" name="spotify" placeholder="spotify username" required />
+                </div>
+                <div className="linksAccountForgot">
+                    <label onClick={goToLogin}>Already have an account? Log in</label>
+                </div>
+                <div className="login-button-container">
+                    <button type="submit">submit</button>
+                </div>
+            </form>
         </div>
     </div>
   );
