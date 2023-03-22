2992676164845a6779880476ea64a038d615f1a3
annsts <an3245@nyu.edu>
2023-03-21 21:09:51 -0400

    user and browse pages

--- front-end/src/Components/UserPost.js
+++ front-end/src/Components/UserPost.js
@@ -1,29 +1,38 @@
 import "./UserPost.css"; 
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Post = () => {
+const UserPost = ({data, post}) => {
+  const navigate = useNavigate();
+  const [likes, setLikes] = useState(0);
+
+  const handleLike = () => {
+    setLikes((prevLikes) => prevLikes + 1);
+  };
+
+  const handlePlay = () => {
+    const query = post.song;
+    const url = `https://www.google.com/search?q=${query}`;
+    window.open(url);
+  };
   return (
     <div className="post">
     <div className="post-header">
-      <img src="image.jpeg" alt="avatar" className="avatar" />
-      <h3>@username</h3>
-    </div>
-    <img src="image.jpeg" alt="post" className="post-image" />
-
-    <div className="link">
-          <a href={"Song Link"} target="_blank" rel="noopener noreferrer">
-            Song
-          </a>
-        </div>
-    <div className="link">
-          <a href={"Location Link"} target="_blank" rel="noopener noreferrer">
-            Location
-          </a>
-        </div>
-    <div className="post-footer">
-      <span>15 likes</span>
-    </div>
+        <img src={data.avatar} alt="avatar" className="avatar" />
+        <h3>@{data.username}</h3>
+      </div>
+      <div className="location" onClick={() => {navigate("/map"); }}> {post.location} </div>
+      <img src={post.image} alt="post" className="post-image" />
+      <div className="song">
+        <button id="play-button" onClick={handlePlay}>Play</button>
+        <div>{post.song}</div>
+      </div>
+      <div className="post-footer">
+        <button id="like-button" onClick={handleLike}>Like</button>
+        <span>{likes} likes</span>
+      </div>
   </div>
   );
 };
 
-export default Post; 
+export default UserPost; 
--- front-end/src/Pages/Browse.js
+++ front-end/src/Pages/Browse.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./Browse.css";
+import axios from "axios";
 import HeaderBrowseMap from "../Components/HeaderBrowseMap";
 import UserPost from "../Components/UserPost"
 
 const Browse = () => {
+    const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    async function fetchData() {
+        const result = await axios(
+          "https://my.api.mockaroo.com/browse.json?key=d0d8c110"
+        );
+        setData(result.data);
+      
+    }
+    fetchData();
+  }, []);
+
     return (
         <div>
         <HeaderBrowseMap/>
+        
             <div className="Browse-items">
                 {/* Posts will go here. They're flexed in a single column */}
-
-                <UserPost/>
-                <UserPost/>
-                <UserPost/>
-                
+                {data.map((post, index) => (<UserPost key={index} data={post} post={post}/>))}
             </div>
         </div>
     );
