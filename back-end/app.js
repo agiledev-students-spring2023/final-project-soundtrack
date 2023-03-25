8e93f62729b395798ba756290e76cf1a670385d4
AliceLong <al6695@nyu.edu>
2023-03-25 12:59:07 -0400

    basic express set up with app.js, server.js, nedemon.

---
+++ back-end/app.js
@@ -0,0 +1,12 @@
+
+
+// import and instantiate express
+const express = require("express") // CommonJS import style!
+const app = express() // instantiate an Express object/server
+// we will put some server logic here later...
+// export the express app we created to make it available to other modules
+module.exports = app
+
+app.get("/", (req, res) => {
+    res.send("thanks !")
+  })
