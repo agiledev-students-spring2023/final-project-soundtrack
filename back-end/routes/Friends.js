b099c5ced0587f16735d2c6969355fbd22d9fc7d
AliceLong <al6695@nyu.edu>
2023-03-29 13:34:26 -0400

    routes organized

---
+++ back-end/routes/Friends.js
@@ -0,0 +1,15 @@
+const express = require("express"); 
+const router = express.Router();
+const axios = require("axios");
+const morgan = require("morgan") 
+
+
+router.get("/", (req, res, next) => {
+    // use axios to make a request to an API for animal data
+    axios
+      .get("https://my.api.mockaroo.com/browse.json?key=d0d8c110")
+      .then(apiResponse => res.json(apiResponse.data)) // pass data along directly to client
+      .catch(err => next(err)) // pass any errors to express
+  })
+
+  module.exports = router;
