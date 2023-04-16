6f432a1eab4b7a4938418341c4b869cd50f4fecc
AliceLong <al6695@nyu.edu>
2023-04-16 00:47:18 -0400

    mongdb atals set up

--- back-end/server.js
+++ back-end/server.js
@@ -1,46 +1,58 @@
 #!/usr/bin/env node
 
-const express = require('express')
+const mongoose = require('mongoose');
+const express = require('express');
 const app = express(); // instantiate an Express object
 const cors = require("cors");
-require('dotenv').config({ silent: true }) // load environmental variables from a hidden file named .env
-const port = process.env.PORT || 3000 // the port to listen to for incoming requests
-app.use(express.json()) // decode JSON-formatted incoming POST data
-app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
+require('dotenv').config({ silent: true }); // load environmental variables from a hidden file named .env
+const port = process.env.PORT || 3000; // the port to listen to for incoming requests
+
+app.use(express.json()); // decode JSON-formatted incoming POST data
+app.use(express.urlencoded({ extended: true })); // decode url-encoded incoming POST data
+
+const userRoute = require('./routes/User');
+const browseRoute = require('./routes/Browse');
+const friendRoute = require('./routes/Friends');
+const locationProfileRoute = require('./routes/LocationProfile');
+const postRoute = require('./routes/Post');
+const loginRoute = require('./routes/Login');
+const createRoute = require('./routes/Account');
+const authRoute = require('./routes/Auth');
 
 
-const userRoute = require('./routes/User')
-const browseRoute = require('./routes/Browse')
-const friendRoute = require('./routes/Friends')
-const locationProfileRoute = require('./routes/LocationProfile')
-const postRoute = require('./routes/Post')
-const loginRoute = require('./routes/Login')
-const createRoute = require('./routes/Account')
-const authRoute = require('./routes/Auth')
+
+
+//why it is not being read in from the .env file?
+
+
+mongoose.connect(process.env.MONG_URL)
+  .then(() => {
+    const server = app.listen(port, function () {
+      console.log(`Server running on port: ${port}`);
+    });
+    console.log('Connected to MongoDB...');
+  })
+  .catch(err => console.error(err + 'Could not connect to MongoDB...'));
 
 app.use(cors());
 const corsOptions = {
   origin: 'http://localhost:7002' // replace with your front-end's URL
-}
-app.use(cors(corsOptions))
-app.use("/user",userRoute);
-app.use("/create",createRoute);
-app.use("/browse",browseRoute);
-app.use("/friends",friendRoute);
-app.use("/locationprofile",locationProfileRoute);
-app.use("/post",postRoute);
-app.use("/",loginRoute);
-app.use("/auth",authRoute);
+};
+app.use(cors(corsOptions));
+app.use("/user", userRoute);
+app.use("/create", createRoute);
+app.use("/browse", browseRoute);
+app.use("/friends", friendRoute);
+app.use("/locationprofile", locationProfileRoute);
+app.use("/post", postRoute);
+app.use("/", loginRoute);
+app.use("/auth", authRoute);
 
 
-// call express's listen function to start listening to the port
-const server = app.listen(port, function () {
-  console.log(`Server running on port: ${port}`)
-})
 
 // a function to stop listening to the port
 const close = () => {
-  server.close()
-}
+  server.close();
+};
 
 module.exports = { app, close };
