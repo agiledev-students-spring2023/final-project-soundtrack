b16e90f644954743482664d1c0fb42ca8c1f3c05
isabellaMas <icm268@nyu.edu>
2023-04-17 13:14:29 -0400

    email working but pass encrypted

--- back-end/routes/Forgot.js
+++ back-end/routes/Forgot.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const secretKey = "shaoxuewenlu";
-const morgan = require("morgan");
 const User = require('../models/User'); // Assuming the model is in a separate file called "userModel.js"
 const nodemailer = require('nodemailer');
+const bcrypt = require('bcrypt');
+    
 
 // Route to log in a user
-router.post('/forgot', async function (req, res) {
-    console.log("in the route");
+router.post('/', async function (req, res) {
   try {
-    console.log("in the route");
 
     const user = await User.findOne({ userName: req.body.username });
     console.log(user);
 
     if (!user || !user.validEmail(req.body.email)) {
-      console.log("cant find user");
       return res.status(401).json({ message: 'Invalid username or email' });
     }
 
@@ -26,16 +21,17 @@ router.post('/forgot', async function (req, res) {
         service: 'gmail',
         auth: {
           user: 'soundtrackwebapp@gmail.com',
-          pass: '<YOUR_GMAIL_PASSWORD>'
+          pass: 'fpwslfvsjvqyovhe'
         }
       });
 
     // Create an email message to send to the user
+    const decryptedPassword = await bcrypt.compare(user.password, user.password);
     const mailOptions = {
       from: 'soundtrackwebapp@gmail.com',
       to: user.email,
       subject: 'Your soundTrack password',
-      text: `Your password is ${user.password}. If you did not request your password, someone is trying to get into your account we recommend changing your password.`
+      text: `Your password is ${decryptedPassword}. If you did not request your password, someone is trying to get into your account we recommend changing your password.`
     };
 
     // Send the email
