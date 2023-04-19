d4b4d9dab7c7ed61a9ca80045600579d546ecdac
annsts <an3245@nyu.edu>
2023-04-19 02:28:40 -0400

    privacy and delete

--- front-end/src/Components/Meatball.js
+++ front-end/src/Components/Meatball.js
@@ -1,20 +1,58 @@
 import React, { useState } from 'react';
 import './Meatball.css';
+import axios from "axios"; 
+import Cookies from "js-cookie";
 
-function Meatball(onDelete) {
+function Meatball({post, postId, onDelete, onPrivacyChange}) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const token = Cookies.get("jwt"); 
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const handleDeleteClick = () => {
-    setIsMenuOpen(false); 
+  const handleDeleteClick = async () => {
+    setIsMenuOpen(false);
+
+    try {
+      const response = await axios.delete(`${process.env.REACT_APP_SERVER_HOSTNAME}/post/deletePost/${postId}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+
+      const data = await response.data;
+      console.log(data);
+      onDelete(postId);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const handleShareClick = () => {
-    setIsMenuOpen(false); 
+  const handlePrivacyClick = async () => {
+    setIsMenuOpen(false);
+  
+    const privacy = post.privacy === "Public" ? "Private" : "Public";
+  
+    try {
+      const response = await axios.patch(`${process.env.REACT_APP_SERVER_HOSTNAME}/post/updatePrivacy/${postId}`, {
+        privacy
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+  
+      const data = await response.data;
+      console.log(data);
+      onPrivacyChange(postId, privacy);
+    } catch (error) {
+      console.error(error);
+    }
   };
+  
 
   return (
     <div className="meatball-menu">
@@ -25,10 +63,9 @@ function Meatball(onDelete) {
             <a onClick={handleDeleteClick}>Delete</a>
           </li>
           <li>
-          <a onClick={handleShareClick}>Share</a>
-          </li>
-          <li>
-          <a onClick={handleShareClick}>Make private</a>
+          <a onClick={handlePrivacyClick}>
+              {post.privacy === "Public" ? "Make private" : "Make public"}
+            </a>
           </li>
         </ul>
       )}
@@ -37,5 +74,3 @@ function Meatball(onDelete) {
 }
 
 export default Meatball;
-
-// Add an icon indicating that the post is private / public 
