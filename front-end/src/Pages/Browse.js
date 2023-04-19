85fe9ab3b6405cc96ffecc5dadc28302243e54d4
annsts <an3245@nyu.edu>
2023-04-19 00:42:17 -0400

    browse and api

--- front-end/src/Pages/Browse.js
+++ front-end/src/Pages/Browse.js
@@ -5,16 +5,16 @@ import HeaderBrowseMap from "../Components/HeaderBrowseMap";
 import UserPost from "../Components/UserPost"
 
 const Browse = () => {
-    const [data, setPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState('');
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_SERVER_HOSTNAME}/browse`)
       .then((result) => {
         console.log(result.data);
-        setPosts(result.data);
+        setData(result.data);
       })
       .catch((err) => {
         setError('Failed to fetch data from the server');
@@ -24,25 +24,22 @@ const Browse = () => {
       });
   }, []);
 
-  
-  
-    return (
-        <div>
-        <HeaderBrowseMap/>
-        
-          
-                {/* Posts will go here. They're flexed in a single column */}
-                {/* {data.map((post, index) => (<UserPost key={index} data={post}/>))} */}
-                {/* {<UserPost data={data[0]}/>} */}
-            <div className="Browse-items">
-              {data.posts &&
-                data.posts
-                  .slice(0, data.posts.length)
-                  .map((post, index) => <UserPost key={index} post={post} />)}
-            </div>
-      
-            </div>
-    );
+  return (
+    <div>
+      <HeaderBrowseMap/>
+      <div className="Browse-items">
+        {loading ? (
+          <div className="loading-message">Loading...</div>
+        ) : error ? (
+          <div className="error-message">{error}</div>
+        ) : (
+          <>
+            {data.map((post, index) => <UserPost key={index} post={post} />)}
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Browse;
