23f42606745676aadde0f3b335670b5484bc5b03
annsts <an3245@nyu.edu>
2023-04-24 05:31:32 -0400

    privacy update all posts

--- back-end/routes/User.js
+++ back-end/routes/User.js
@@ -127,7 +127,7 @@ router.patch("/username", authenticateToken, async (req, res) => {
 
 router.patch("/privacy", authenticateToken, async (req, res) => {
   const userId = req.user.id;
-  const newPrivacy = req.body.privacy;
+  const newPrivacyBool = req.body.privacy;
 
   const user = await User.findOne({ userId: userId });
 
@@ -135,7 +135,10 @@ router.patch("/privacy", authenticateToken, async (req, res) => {
     return res.status(404).json({ error: "User not found" });
   }
 
-  user.privacy = newPrivacy;
+  user.privacy = newPrivacyBool;
+  const newPrivacy = newPrivacyBool ? 'Private' : 'Public';
+  await Post.updateMany({ userId: userId }, { privacy: newPrivacy });
+
   await user.save();
 
   res
