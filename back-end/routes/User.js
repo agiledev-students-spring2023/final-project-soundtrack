57e2176129bc831f69f1ac1b5c182c055d35fb01
isabellaMas <icm268@nyu.edu>
2023-05-01 13:29:59 -0400

    file names

--- back-end/routes/User.js
+++ back-end/routes/User.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const Post = require("../models/post"); // or whatever your post model is called
+const Post = require("../models/Post"); // or whatever your post model is called
 const jwt = require("jsonwebtoken");
 const secretKey = "shaoxuewenlu";
 const User = require("../models/User"); // Assuming the model is in a separate file called "userModel.js"
