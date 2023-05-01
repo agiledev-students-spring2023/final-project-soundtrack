0e8b64e3f3edd024079f35081fb6caf01f4b3b21
annsts <an3245@nyu.edu>
2023-05-01 00:58:05 -0400

    infinite scroll

--- front-end/src/Pages/Browse.js
+++ front-end/src/Pages/Browse.js
@@ -3,6 +3,8 @@ import "./Browse.css";
 import axios from "axios";
 import HeaderBrowseMap from "../Components/HeaderBrowseMap";
 import UserPost from "../Components/UserPost";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { debounce } from "lodash";
 
 const Browse = () => {
   const [data, setData] = useState([]);
@@ -11,56 +13,57 @@ const Browse = () => {
   const [skip, setSkip] = useState(0);
   const [limit, setLimit] = useState(3);
   const [hasMore, setHasMore] = useState(true);
-  const [isLoadingMore, setIsLoadingMore] = useState(false);
 
-  const loadMore = useCallback(() => {
-    setIsLoadingMore(true);
-    axios
-      .get(`${process.env.REACT_APP_SERVER_HOSTNAME}/browse/${skip}/${limit}`)
-      .then((result) => {
-        setData((prevData) => {
-          const uniquePosts = result.data.filter((post) => !prevData.some((p) => p._id === post._id));
-          return [...prevData, ...uniquePosts];
+  const loadMore = useCallback(
+    debounce(() => {
+      axios
+        .get(`${process.env.REACT_APP_SERVER_HOSTNAME}/browse/${skip}/${limit}`)
+        .then((result) => {
+          setData((prevData) => {
+            const uniquePosts = result.data.filter((post) => !prevData.some((p) => p._id === post._id));
+            return [...prevData, ...uniquePosts];
+          });
+          setHasMore(result.data.length > 0);
+          setSkip((prevSkip) => prevSkip + limit);
+        })
+        .catch((err) => {
+          setError("Failed to fetch data from the server");
+        })
+        .finally(() => {
+          setLoading(false);
         });
-        setHasMore(result.data.length > 0);
-      })
-      .catch((err) => {
-        setError("Failed to fetch data from the server");
-      })
-      .finally(() => {
-        setIsLoadingMore(false);
-        setLoading(false);
-      });
-  }, [skip, limit]);
+    }, 500),
+    [skip, limit]
+  );
+  
 
   useEffect(() => {
     loadMore();
   }, [loadMore]);
 
-  const handleLoadMore = (e) => {
-    e.preventDefault();
-    if (!isLoadingMore) {
-      setSkip((prevSkip) => prevSkip + limit);
-    }
-  };
-
   return (
     <div>
       <HeaderBrowseMap />
       <div className="Browse-items">
-        {data.map((post) => (
-          <UserPost key={post._id} post={post} />
-        ))}
-        {/* {loading && <div className="loading-message">Loading...</div>} */}
+        <InfiniteScroll
+          dataLength={data.length}
+          next={loadMore}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>You have seen it all</b>
+            </p>
+          }
+        >
+          {data.map((post) => (
+            <UserPost key={post._id} post={post} />
+          ))}
+        </InfiniteScroll>
         {error && <div className="error-message">{error}</div>}
-        {hasMore && (
-          <a href="#" onClick={handleLoadMore} disabled={isLoadingMore}>
-            {isLoadingMore ? "Loading..." : "Load More"}
-          </a>
-        )}
       </div>
     </div>
   );
 };
 
-export default Browse;
+export default Browse; 
